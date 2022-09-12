d9733b9291e35c12945ea1abdb75e44258bf51e3
Zach Ruiz <powersbooth10@gmail.com>
2022-09-12 09:09:47 -0500

    commit tree ce80c39751853ed18ebc23cee3b72fea3b274a12

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = document.querySelector('input#searchByID');
+        console.log(input.value);
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((res) => res.json())
+        .then((data) => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.textContent = data.title;
+            summary.textContent = data.summary;
+        });
+
+    });
   
 }
 
